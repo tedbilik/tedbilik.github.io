045e158f79ae69b09aaeb4f95f39c3ff29780d73
Abutalib-Barish Namazov <abutalib@mit.edu>
2023-01-18 01:11:35 +0400

    a lot of work

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
